145d4e1c7b768d0496b08e745d3a805c0e14bf66
gjrmd953 <gjrmd953@gmail.com>
2026-01-28 23:33:08 +0600

    edit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,11 @@ const Navbar = () => {
   return (
     <div>
         <Container>
-            <h2>hello</h2>
+            <div className="w-1/4">
+             <h1>GJR</h1>
+            </div>
+            <div className="w-2/4"></div>
+            <div className="w-1/4"></div>
         </Container>
     </div>
   )
