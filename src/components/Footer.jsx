40aee4341ba932cf38683ea080bb3cedb42eab36
gjrmd953 <gjrmd953@gmail.com>
2026-02-11 23:25:55 +0600

    edit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Container from './Container'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='py-4 bg-[#d1d1]'>
+        <Container>
+            <div className="">
+                <h2>
+                    hello
+                </h2>
+            </div>
+        </Container>
+    </div>
   )
 }
 
