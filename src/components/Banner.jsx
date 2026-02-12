5021d87444025583db165263ca9f31392b937c78
gjrmd953 <gjrmd953@gmail.com>
2026-02-12 23:47:55 +0600

    edit

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,13 +1,25 @@
+import Container from './Container'
 import React from 'react'
 
 const Banner = () => {
-  return (
-    <div>
-        <h2>
-            hello
-        </h2>
-    </div>
-  )
+    return (
+        <div>
+            <Container>
+                <h2>
+                    hello
+                </h2>
+                <h2>
+                        hi im md jashim uddin
+                    </h2>
+                <div className="">
+
+                    <h2>
+                        hi im md jashim uddin
+                    </h2>
+                </div>
+            </Container>
+        </div>
+    )
 }
 
 export default Banner
